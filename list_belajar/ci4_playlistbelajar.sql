a3156cbc90fb6ead30fe5cc6ec37fb1b9127074d
afar0308 <afdhalarrazy111@gmail.com>
2021-04-15 13:53:03 +0700

    Add database backup

--- list_belajar/ci4_playlistbelajar.sql
+++ list_belajar/ci4_playlistbelajar.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 11 Apr 2021 pada 15.47
--- Versi server: 10.4.17-MariaDB
--- Versi PHP: 7.4.13
+-- Host: localhost
+-- Generation Time: Apr 15, 2021 at 08:52 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.4.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `bookmark`
+-- Table structure for table `bookmark`
 --
 
 CREATE TABLE `bookmark` (
@@ -34,23 +34,39 @@ CREATE TABLE `bookmark` (
   `marked_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `bookmark`
+--
+
+INSERT INTO `bookmark` (`id_bookmark`, `id_akun`, `id_playlist`, `marked_at`) VALUES
+(30, 1, 3, '2021-04-15 13:12:00');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `konten`
+-- Table structure for table `konten`
 --
 
 CREATE TABLE `konten` (
   `id_materi` int(11) NOT NULL,
   `id_tipe` int(11) NOT NULL,
   `id_file` int(11) NOT NULL,
-  `nama_file` varchar(255) DEFAULT NULL
+  `nama_file` varchar(255) DEFAULT NULL,
+  `link` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `konten`
+--
+
+INSERT INTO `konten` (`id_materi`, `id_tipe`, `id_file`, `nama_file`, `link`) VALUES
+(5, 4, 5, NULL, 'https://www.youtube.com/embed/zpOULjyy-n8?rel=0'),
+(6, 2, 6, 'CV.pdf', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `materi`
+-- Table structure for table `materi`
 --
 
 CREATE TABLE `materi` (
@@ -61,10 +77,18 @@ CREATE TABLE `materi` (
   `updated_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `materi`
+--
+
+INSERT INTO `materi` (`id_playlist`, `id_materi`, `id_tipe`, `nama_materi`, `updated_at`) VALUES
+(2, 5, 4, 'Video 1', '2021-04-14 10:40:14'),
+(2, 6, 2, 'Catatan', '2021-04-14 17:23:11');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `playlist_belajar`
+-- Table structure for table `playlist_belajar`
 --
 
 CREATE TABLE `playlist_belajar` (
@@ -79,10 +103,19 @@ CREATE TABLE `playlist_belajar` (
   `updated_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `playlist_belajar`
+--
+
+INSERT INTO `playlist_belajar` (`id_akun`, `id_playlist`, `profile_pict`, `nama_playlist`, `kategori`, `deskripsi`, `views`, `created_at`, `updated_at`) VALUES
+(1, 2, 'alligator.png', 'DNA adalah jalan hidupku', 'Biologi', 'Ada', 143, '2021-04-14 10:11:47', '2021-04-14 10:40:14'),
+(4, 3, 'web_design.png', 'Organ', 'Biologi', 'Halo', 17, '2021-04-14 11:06:56', '2021-04-14 11:06:56'),
+(5, 4, 'web_design_1.png', 'Buaya', 'Bahasa Indonesia', 'Aku adalah anak gembala', 17, '2021-04-14 11:12:37', '2021-04-14 11:12:37');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tipe`
+-- Table structure for table `tipe`
 --
 
 CREATE TABLE `tipe` (
@@ -91,7 +124,7 @@ CREATE TABLE `tipe` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `tipe`
+-- Dumping data for table `tipe`
 --
 
 INSERT INTO `tipe` (`id_tipe`, `tipe`) VALUES
@@ -105,56 +138,56 @@ INSERT INTO `tipe` (`id_tipe`, `tipe`) VALUES
 --
 
 --
--- Indeks untuk tabel `bookmark`
+-- Indexes for table `bookmark`
 --
 ALTER TABLE `bookmark`
   ADD PRIMARY KEY (`id_bookmark`);
 
 --
--- Indeks untuk tabel `konten`
+-- Indexes for table `konten`
 --
 ALTER TABLE `konten`
   ADD PRIMARY KEY (`id_file`);
 
 --
--- Indeks untuk tabel `materi`
+-- Indexes for table `materi`
 --
 ALTER TABLE `materi`
   ADD PRIMARY KEY (`id_materi`);
 
 --
--- Indeks untuk tabel `playlist_belajar`
+-- Indexes for table `playlist_belajar`
 --
 ALTER TABLE `playlist_belajar`
   ADD PRIMARY KEY (`id_playlist`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `bookmark`
+-- AUTO_INCREMENT for table `bookmark`
 --
 ALTER TABLE `bookmark`
-  MODIFY `id_bookmark` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_bookmark` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
--- AUTO_INCREMENT untuk tabel `konten`
+-- AUTO_INCREMENT for table `konten`
 --
 ALTER TABLE `konten`
-  MODIFY `id_file` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_file` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT untuk tabel `materi`
+-- AUTO_INCREMENT for table `materi`
 --
 ALTER TABLE `materi`
-  MODIFY `id_materi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_materi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT untuk tabel `playlist_belajar`
+-- AUTO_INCREMENT for table `playlist_belajar`
 --
 ALTER TABLE `playlist_belajar`
-  MODIFY `id_playlist` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_playlist` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
